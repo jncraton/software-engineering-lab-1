35bd2ee4b7f1937de59017aa6fc5002b92cb1436
jepoorbaugh <jepoorbaugh@gmail.com>
2024-08-30 11:00:36 -0400

    Addded Powerups and the ability to have multiple ravens

--- main.js
+++ main.js
@@ -11,11 +11,16 @@ const brickHeight = 20
 const brickPadding = 10
 const brickOffsetTop = 30
 const brickOffsetLeft = 20
+const powerupHitbox = 8
+const powerupColor = '#7700ff'
+const powerupProbability = 0.33
 const color = '#f57920'
 let ravensHarmed = 0
 let ravensFreed = 0
 let paused = true
 const bricks = []
+let ravens = []
+let powerups = []
 
 function loadImage (src) {
   const img = new window.Image()
@@ -36,18 +41,31 @@ document.querySelector('#pause').addEventListener('click', togglePause)
 document.querySelector('#step').addEventListener('click', () => {
   if (paused) { draw() }
   console.clear()
-  console.log(`x: ${x}\ndx: ${dx}\ny: ${y}\ndy: ${dy}`)
+  for(let r = 0; r < ravens.length; r++) {
+    console.log(`Raven ${r}: x: ${ravens[r].x}\ndx: ${ravens[r].dx}\ny: ${ravens[r].y}\ndy: ${ravens[r].dy}`)
+  }
 })
+
+// function addRaven() {
+//   const difficulty = document.querySelector('input[name=difficulty]').value
+//   dx = difficulty * (2.5 + Math.random()) / 4
+//   dy = -(difficulty * difficulty - dx * dx)
+//   ravens.push({x: canvas.width / 2, y: canvas.height - 30, dx: dx, dy: dy, status: 1})
+// }
 
 function restart () {
   const difficulty = document.querySelector('input[name=difficulty]').value
 
-  x = canvas.width / 2
-  y = canvas.height - 30
+  powerups = []
+  ravens = []
+  ravens.push({x: canvas.width / 2, y: canvas.height - 30, dx: 0, dy: 0, status: 1})
+  // x = canvas.width / 2
+  // y = canvas.height - 30
+
   paddleX = (canvas.width - paddleWidth) / 2
 
-  dx = 0
-  dy = 0
+  // dx = 0
+  // dy = 0
 
   for (let c = 0; c < brickColumnCount; c++) {
     bricks[c] = []
@@ -58,8 +76,12 @@ function restart () {
 
   clearTimeout(restart.timer)
   restart.timer = setTimeout(() => {
-    dx = difficulty * (2.5 + Math.random()) / 4
-    dy = -(difficulty * difficulty - dx * dx)
+    for(let r = 0; r < ravens.length; r++) {
+      ravens[r].dx = difficulty * (2.5 + Math.random()) / 4
+      ravens[r].dy = -(difficulty * difficulty - ravens[r].dx * ravens[r].dx)
+      console.log(ravens[r].dy)
+    }
+    
   }, 1000)
 }
 document.querySelector('#restart').addEventListener('click', restart)
@@ -76,7 +98,11 @@ document.addEventListener('mousemove', movePaddle, false)
 document.addEventListener('touchmove', movePaddle, false)
 
 function drawRaven () {
-  ctx.drawImage(ravenRight, x - 38, y - 12)
+  for(let r = 0; r < ravens.length; r++) {
+    if (ravens[r].status === 1) {
+      ctx.drawImage(ravenRight, ravens[r].x - 38, ravens[r].y - 12)
+    }
+  }
 }
 
 function drawPaddle () {
@@ -109,14 +135,30 @@ function drawScore () {
   document.querySelector('#score').textContent = 'Ravens Freed: ' + ravensFreed + ' Ravens Harmed: ' + ravensHarmed
 }
 
+function drawPowerups () {
+  for (let p = 0; p < powerups.length; p++) {
+    if (powerups[p].status === 0) continue
+    ctx.beginPath()
+    ctx.ellipse(powerups[p].x, powerups[p].y, powerupHitbox, powerupHitbox, 0, 0, 2 * Math.PI)
+    ctx.fillStyle = powerupColor
+    ctx.fill()
+    ctx.closePath()
+  }
+}
+
 function collisionDetection () {
-  for (let c = 0; c < brickColumnCount; c++) {
-    for (let r = 0; r < brickRowCount; r++) {
-      const b = bricks[c][r]
-      if (b.status === 1) {
-        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-          dy = -dy
-          b.status = 0
+  for (let i = 0; i < ravens.length; i++) {
+    for (let c = 0; c < brickColumnCount; c++) {
+      for (let r = 0; r < brickRowCount; r++) {
+        const b = bricks[c][r]
+        if (b.status === 1) {
+          if (ravens[i].x > b.x && ravens[i].x < b.x + brickWidth && ravens[i].y > b.y && ravens[i].y < b.y + brickHeight) {
+            ravens[i].dy = -ravens[i].dy
+            b.status = 0
+            if (Math.random() < powerupProbability) {
+              powerups.push({x: b.x + brickWidth / 2, y: b.y + brickHeight / 2, dy: 1, status: 1})
+            }
+          }
         }
       }
     }
@@ -127,28 +169,69 @@ function draw () {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   collisionDetection()
 
-  if (x + dx > canvas.width - ravenHitbox || x + dx < ravenHitbox) {
-    dx = -dx
+  for(let r = 0; r < ravens.length; r++) {
+    if (ravens[r].status === 1) {
+      if (ravens[r].x + ravens[r].dx > canvas.width - ravenHitbox || ravens[r].x + ravens[r].dx < ravenHitbox) {
+        ravens[r].dx = -ravens[r].dx
+      }
+
+      if (ravens[r].y < -100) {
+        ravensFreed++
+        ravens[r].status = 0
+        // restart()
+      } else if (ravens[r].y > canvas.height + 64) {
+        ravensHarmed++
+        ravens[r].status = 0
+        // restart()
+      } else if (ravens[r].y > canvas.height - ravenHitbox 
+              && ravens[r].y < canvas.height + ravenHitbox 
+              && ravens[r].x > paddleX 
+              && ravens[r].x < paddleX + paddleWidth 
+              && ravens[r].dy > 0) {
+        ravens[r].dy = -ravens[r].dy
+      }
+    
+      ravens[r].x += ravens[r].dx
+      ravens[r].y += ravens[r].dy
+    }
   }
 
-  if (y < -100) {
-    ravensFreed++
-    restart()
-  } else if (y > canvas.height + 64) {
-    ravensHarmed++
-    restart()
-  } else if (y > canvas.height - ravenHitbox && y < canvas.height + ravenHitbox &&
-            x > paddleX && x < paddleX + paddleWidth && dy > 0) {
-    dy = -dy
+  let allRavensGone = true
+  for(let r = 0; r < ravens.length; r++) {
+    if (ravens[r].status === 1) {
+      allRavensGone = false
+    }
   }
 
-  x += dx
-  y += dy
+  for(let p = 0; p < powerups.length; p++) {
+    if (powerups[p].status === 0) continue
+    powerups[p].y += powerups[p].dy
+    if (powerups[p].y > canvas.height - ravenHitbox 
+      && powerups[p].y < canvas.height + ravenHitbox 
+      && powerups[p].x > paddleX 
+      && powerups[p].x < paddleX + paddleWidth 
+      && powerups[p].dy > 0) {
+        powerups[p].status = 0
+        
+        const difficulty = document.querySelector('input[name=difficulty]').value
+        dx = difficulty * (2.5 + Math.random()) / 4
+        dy = -(difficulty * difficulty - dx * dx)
+        ravens.push({x: paddleX + paddleWidth / 2, y: canvas.height - 30, dx: dx, dy: dy, status: 1})
+        ravens.push({x: paddleX + paddleWidth / 2, y: canvas.height - 30, dx: -dx, dy: dy, status: 1})
+    }
+
+  }
+
+  if (allRavensGone) {
+    restart()
+  }
 
   drawBricks()
   drawRaven()
+  drawPowerups()
   drawPaddle()
   drawScore()
+  
 
   if (!paused) {
     window.requestAnimationFrame(draw)
